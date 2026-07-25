703378243f8591a3657ff3ebc50c5861a38dd4a1
Apostolidis Christos <it2021010@hua.gr>
2026-07-26 01:54:57 +0300

    Allow CORS requests from Docker frontend (port 9000)

--- src/main/java/com/hua/myElga/config/SecurityConfig.java
+++ src/main/java/com/hua/myElga/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         final CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:9000"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE"));
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setExposedHeaders(List.of("Authorization"));
